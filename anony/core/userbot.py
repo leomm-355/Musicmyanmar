8fa6e7597ed4f8de3e9750e61a57e6f3f5a26efd
leomm-355 <leomax24407@gmail.com>
2026-09-24 08:14:07 +0630

    Update userbot.py

--- anony/core/userbot.py
+++ anony/core/userbot.py
@@ -49,7 +49,7 @@ class Userbot(Client):
         client = clients[num]
         await client.start()
         try:
-            await client.send_message(config.LOGGER_ID, "Assistant Now Ready YanYan_❍ᴡηєʀ")
+            await client.send_message(config.LOGGER_ID, "Assistant Now ")
         except Exception:
             raise SystemExit(f"Assistant {num} failed to send message in log group.")
 
